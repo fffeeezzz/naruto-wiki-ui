8b29f28d307b5175ab106f5f670e2082aeea52bd
Denis Portnov <portnov.denis@list.ru>
2024-03-01 18:22:18 +0300

    master add random auth view

--- src/scripts/logo_gif.js
+++ src/scripts/logo_gif.js
@@ -13,3 +13,21 @@ window.addEventListener('load', () => {
         imgElem.setAttribute('src', prevSrc);
     }, 2000)
 })
+
+
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+window.addEventListener('load', () => {
+    if (getRandomInt(0, 10) % 2 === 0) {
+        const headerAuth = document.querySelector('.header-auth')
+        const signIn = document.querySelector('#sign-in')
+        const register = document.querySelector('#register')
+        headerAuth.removeChild(signIn)
+        headerAuth.removeChild(register)
+
+        const img = document.createElement('img');
+        img.src = 'src/assets/icons/auth-icon.png';
+        headerAuth.append(img)
+    }
+})
